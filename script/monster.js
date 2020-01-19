0be6cf9fbd4d7adb04c0f02ba3a9ad53f2b8ddac
FiglerReNi <renatafigler85@gmail.com>
2020-01-19 17:07:39 +0100

    forras

--- script/monster.js
+++ script/monster.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     window.onblur = lightningStop;
     window.onfocus = lightningOn;
 
-    var clixArray = [0,0,0,0];
+    var clixArray = [0, 0, 0, 0];
     var one;
     var two;
     var three;
@@ -21,7 +21,10 @@ $(document).ready(function () {
     $('#mouth').click(function () {
         clixArray[3] = clickImg(clixArray[3], this);
     });
-});
+
+    $('#btnRandom').click(randomize);
+    $('#btnReset').click(reset);
+
 
 //A verzió
 // function lightning_one(t){
@@ -30,45 +33,77 @@ $(document).ready(function () {
 // };
 
 //B verzió
-function lightning_one(){
-    $('#lightning1').fadeIn(250).fadeOut(250);
-}
-function lightning_two(){
-    $('#lightning2').fadeIn(250).fadeOut(250);
-}
-function lightning_three(){
-    $('#lightning3').fadeIn(250).fadeOut(250);
-}
-
-function lightningStop(){
-    clearInterval(one);
-    clearInterval(two);
-    clearInterval(three);
-}
-
-function lightningOn(){
-     one = setInterval(lightning_one, 4000);
-     two = setInterval(lightning_two, 5000);
-     three = setInterval(lightning_three, 7000);
-}
-
-function animation(item, a){
-    $(item).animate({left:a}, 500);
-}
-
-function clickImg(clix, item) {
-    if (clix < 9) {
-        //relative position
-        animation(item, "-=367px");
-        clix++;
-        return clix;
-    } else {
-        //absolute position
-        animation(item, "0px");
-        clix = 0;
-        return clix;
+    function lightning_one() {
+        $('#lightning1').fadeIn(250).fadeOut(250);
     }
-}
+
+    function lightning_two() {
+        $('#lightning2').fadeIn(250).fadeOut(250);
+    }
+
+    function lightning_three() {
+        $('#lightning3').fadeIn(250).fadeOut(250);
+    }
+
+    function lightningStop() {
+        clearInterval(one);
+        clearInterval(two);
+        clearInterval(three);
+    }
+
+    function lightningOn() {
+        one = setInterval(lightning_one, 4000);
+        two = setInterval(lightning_two, 5000);
+        three = setInterval(lightning_three, 7000);
+    }
+
+    function animation(item, a) {
+        $(item).animate({left: a}, 500);
+    }
+
+    function clickImg(clix, item) {
+        if (clix < 9) {
+            //relative position
+            animation(item, "-=367px");
+            clix++;
+            return clix;
+        } else {
+            //absolute position
+            animation(item, "0px");
+            clix = 0;
+            return clix;
+        }
+    }
+
+    function getRandom() {
+        var number = Math.floor(Math.random() * 10);
+        return number;
+    }
+
+    function randomize() {
+        debugger;
+        $('.face').each(function(index) {
+            var currentPoz = clixArray[index];
+            var targetPoz = getRandom();
+            clixArray[index] = targetPoz;
+            if(currentPoz < targetPoz){
+                let num = (targetPoz - currentPoz)*367;
+                animation(this, "-="+num);
+            }
+            else if(currentPoz > targetPoz){
+                let num = (currentPoz - targetPoz)*367;
+                animation(this, "+="+num);
+            }
+        });
+    }
+
+    function reset(){
+        $('.face').each(function(index) {
+            animation(this, '0px');
+            clixArray[index] = 0;
+        });
+    }
+});
 //effects
 //CSS opacity property
 //fadeIn - slow, normal, fast, miliseconds
@@ -104,3 +139,4 @@ window.setInterval()
 window.clearTimeout()
 window.clearInterval()
  */
+//window.top.close() - a main/szülő ablakot zárja be (ha időözben elnavigálunk egy másik oldalra )
